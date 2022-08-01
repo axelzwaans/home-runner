4f20b15d20903a8473e2aa84cd364f2150e83eb1
Axel Zwaans <axelzwaans@hotmail.com>
2022-08-01 13:37:39 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,37 +1,20 @@
-print("Welcome to my computer quiz")
-playing = input('Do you want to play? '.lower())
-if playing != 'yes':
-    quit()
+from time import gmtime, strftime
 
-print("Okay, let's play!")
-score = 0
+clock = strftime ("%a, %d %b %Y and the time is %H:%M:%S", gmtime())
 
-answer = input("What does CPU stand for? ")
-if answer.lower() == "central processing unit":
-    print('Correct!')
-    score += 1
+def intro():
+    answer = input(f"It's {clock}, you're in Amsterdam and you just realised you were supposed to be in Dublin for a family event by the evening")
+
+
+character = input("What is your name, traveller? ")
+answer = input(f'Hello {character}, are you ready for your journey to London? (Y/N) ')
+
+
+if answer.lower().strip() == "y":
+    intro()
+    
+
 else:
-    print("Incorrect")
+    print("That's understandable, flying isn't what it used to be anyway")
+        
 
-answer = input("What does GPU stand for? ")
-if answer.lower() == "graphics processing unit":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect")
-
-answer = input("What does RAM stand for? ")
-if answer.lower() == "random access memory":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect")
-
-answer = input("What does PSU stand for? ")
-if answer.lower() == "power supply":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect")
-
-print(f'Your score is {score}')
